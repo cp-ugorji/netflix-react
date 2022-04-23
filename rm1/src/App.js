393386df6263b2db3dace8d759dbfe53030f7c31
Chukwudi Ugorji <32703051+cp-ugorji@users.noreply.github.com>
2022-04-23 18:11:59 +0100

    created a pure class component to perform search

--- rm1/src/App.js
+++ rm1/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import Counter from './components/Counter';
+import Search from './components/Search';
 
 function App() {
   return (
     <div className="App">
       {/* react class component */}
       <Counter/>
+
+      {/* react pure class component */}
+      <Search />
     </div>
   );
 }
