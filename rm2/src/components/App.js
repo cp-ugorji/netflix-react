230dcdbb83c4950895bf7619ea609f8e19736e76
Chukwudi Ugorji <32703051+cp-ugorji@users.noreply.github.com>
2022-04-27 16:57:02 +0100

    added prettier config

--- rm2/src/components/App.js
+++ rm2/src/components/App.js
@@ -1,9 +1,7 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export default class App extends Component {
   render() {
-    return (
-      <div>My React App</div>
-    )
+    return <div>My React App</div>;
   }
 }
