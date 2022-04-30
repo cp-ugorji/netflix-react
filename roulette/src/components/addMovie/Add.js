cc8ddd043fcc638abbcd87d765aff6684842d868
Chukwudi Ugorji <32703051+cp-ugorji@users.noreply.github.com>
2022-04-30 09:31:44 +0100

    added add movie button

---
+++ roulette/src/components/addMovie/Add.js
@@ -0,0 +1,11 @@
+const Add = () => {
+  return (
+    <div className="new-movie-container">
+      <button type="button" className="btn-add" onClick={open}>
+        + ADD MOVIE
+      </button>
+    </div>
+  );
+};
+
+export default Add;
