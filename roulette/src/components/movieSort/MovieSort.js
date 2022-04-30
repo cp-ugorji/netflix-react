07dba42686e9f825129bc6c16d5c5c74a2de5375
Chukwudi Ugorji <32703051+cp-ugorji@users.noreply.github.com>
2022-04-30 10:51:40 +0100

    basic markup for home page done

---
+++ roulette/src/components/movieSort/MovieSort.js
@@ -0,0 +1,19 @@
+import './MovieSort.css';
+import { Listbox, ListboxOption } from '@reach/listbox';
+import '@reach/listbox/styles.css';
+
+const MovieSort = ({ sortMovies }) => {
+  return (
+    <div className="sort-content">
+      <span>SORT BY</span>
+      <Listbox aria-labelledby="sort-by">
+        <ListboxOption value="release_date">RELEASE DATE</ListboxOption>
+        <ListboxOption value="title">TITLE</ListboxOption>
+        <ListboxOption value="vote_count">VOTE COUNT</ListboxOption>
+        <ListboxOption value="vote_average">VOTE AVERAGE</ListboxOption>
+      </Listbox>
+    </div>
+  );
+};
+
+export default MovieSort;
