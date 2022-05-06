246c97214fb90663daa18a8090048b12c51d9ccb
Chukwudi Ugorji <32703051+cp-ugorji@users.noreply.github.com>
2022-05-06 15:16:15 +0100

    removed logic from jsx

--- src/components/movieList/MovieList.js
+++ src/components/movieList/MovieList.js
@@ -1,31 +1,36 @@
 import './MovieList.css';
 import Movie from '../movieItem/Movie';
 
-const MovieList = ({ movies }) => {
-  return (
-    <>
-      {movies.length ? (
-        <>
-          <h1>
-            {movies.length}
-            {movies.length > 1 ? ' movies' : ' movie'} found
-          </h1>
-          <div className="movie-list-content">
-            {movies.map((movie) => (
-              <Movie key={movie.id} movie={movie} />
-            ))}
-          </div>
-        </>
-      ) : (
-        <div className="movies-not-found">
-          <h1>No movie found</h1>
-          <button type="button" className="btn-confirm">
-            GO BACK TO SEARCH
-          </button>
+const movieContent = (movies) => {
+  if(movies.length){
+    return (
+      <>
+        <h1>
+          {movies.length}
+          {movies.length > 1 ? ' movies' : ' movie'} found
+        </h1>
+        <div className="movie-list-content">
+          {movies.map((movie) => (
+            <Movie key={movie.id} movie={movie} />
+          ))}
         </div>
-      )}
-    </>
-  );
+      </>
+    );
+  }
+  else{
+    return (
+      <div className="movies-not-found">
+        <h1>No movie found</h1>
+        <button type="button" className="btn-confirm">
+          GO BACK TO SEARCH
+        </button>
+      </div>
+    )
+  }
+}
+
+const MovieList = ({ movies }) => {
+  return movieContent(movies);
 };
 
 export default MovieList;
