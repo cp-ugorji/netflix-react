0d7bfe63a2cf48faddbf4c6d71a670368069687a
Chukwudi Ugorji <32703051+cp-ugorji@users.noreply.github.com>
2022-05-08 00:50:12 +0100

    WIP - page no longer loading

--- src/components/modals/MovieForm.js
+++ src/components/modals/MovieForm.js
@@ -1,6 +1,3 @@
-// import { DialogContent, DialogOverlay } from '@reach/dialog';
-// import '@reach/dialog/styles.css';
-// import PropTypes from 'prop-types';
 
 const MovieForm = () => {
   return <div>movieForm</div>;
